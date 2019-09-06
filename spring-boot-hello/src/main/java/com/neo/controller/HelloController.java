56cb5db0ca40f869327e99ab9162e0219f53aae1
paul.cheng <paul.cheng>
2019-09-06 11:47:15 +0800

    配置修改

--- spring-boot-hello/src/main/java/com/neo/controller/HelloController.java
+++ spring-boot-hello/src/main/java/com/neo/controller/HelloController.java
@@ -1,11 +1,22 @@
 package com.neo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	
+
+    @Value("${com.zzq.name}")
+    private  String name;
+    @Value("${com.zzq.title}")
+    private  String title;
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return title + "：" + name;
+    }
+
     @RequestMapping("/")
     public String index() {
         return "Hello Spring Boot 2.0!";
